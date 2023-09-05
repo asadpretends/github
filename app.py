52e5b9dce91bbe59df5dfab0075ededff7b688a0
asadpretends <90529955+asadpretends@users.noreply.github.com>
2023-09-05 22:49:11 +0500

    Update app.py

--- app.py
+++ app.py
@@ -16,34 +16,7 @@ def divide(x, y):
 
 
 # Main calculator loop
-while True:
-    print("Options:")
-    print("Enter 'add' for addition")
-    print("Enter 'subtract' for subtraction")
-    print("Enter 'multiply' for multiplication")
-    print("Enter 'divide' for division")
-    print("Enter 'quit' to end the program")
-    
-   # user_input = input(": ")
-    
- #   if user_input == "quit":
-  #      break
-   # elif user_input in ["add", "subtract", "multiply", "divide"]:
-    #    num1 = float(input("Enter first number: "))
-     #   num2 = float(input("Enter second number: "))
-        
-      #  if user_input == "add":
-       #     print("Result:", add(num1, num2))
-        #elif user_input == "subtract":
-         #   print("Result:", subtract(num1, num2))
-        #elif user_input == "multiply":
-         #   print("Result:", multiply(num1, num2))
-      #  elif user_input == "divide":
-       #     print("Result:", divide(num1, num2))
-    #else:
-     #   print("Invalid input. Please enter a valid option.")
-
-
-
-
-
+add(1,2)
+subtract(4,1)
+multiply(5,6)
+divide(15,5)
