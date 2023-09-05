dab807d8ddd7d1baaec82543936913dfc13e2139
asadpretends <90529955+asadpretends@users.noreply.github.com>
2023-09-05 22:59:46 +0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -2,3 +2,12 @@ from app import add
 
 def test_add():
     assert 2 == add(1,1)
+
+def test_subtract():
+    assert 0 == sub(1,1)
+
+def test_multiply():
+    assert 10 == multiply(1,10)
+
+def test_divide():
+    assert 3 == divide(5,15)
