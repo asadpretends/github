6b944bb38c329dd38135cdd0e6cc11c4f63721c5
asadpretends <90529955+asadpretends@users.noreply.github.com>
2023-09-05 22:30:13 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,24 +11,24 @@ while True:
     print("Enter 'divide' for division")
     print("Enter 'quit' to end the program")
     
-    user_input = input(": ")
+   # user_input = input(": ")
     
-    if user_input == "quit":
-        break
-    elif user_input in ["add", "subtract", "multiply", "divide"]:
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
+ #   if user_input == "quit":
+  #      break
+   # elif user_input in ["add", "subtract", "multiply", "divide"]:
+    #    num1 = float(input("Enter first number: "))
+     #   num2 = float(input("Enter second number: "))
         
-        if user_input == "add":
-            print("Result:", add(num1, num2))
-        elif user_input == "subtract":
-            print("Result:", subtract(num1, num2))
-        elif user_input == "multiply":
-            print("Result:", multiply(num1, num2))
-        elif user_input == "divide":
-            print("Result:", divide(num1, num2))
-    else:
-        print("Invalid input. Please enter a valid option.")
+      #  if user_input == "add":
+       #     print("Result:", add(num1, num2))
+        #elif user_input == "subtract":
+         #   print("Result:", subtract(num1, num2))
+        #elif user_input == "multiply":
+         #   print("Result:", multiply(num1, num2))
+      #  elif user_input == "divide":
+       #     print("Result:", divide(num1, num2))
+    #else:
+     #   print("Invalid input. Please enter a valid option.")
 
 
 
